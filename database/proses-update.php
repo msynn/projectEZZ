8fb9879720dfadfbbc62b92ea33549e7e48f03eb
msynn <fyanramadhan1@gmail.com>
2022-04-27 08:19:16 +0930

    update

--- database/proses-update.php
+++ database/proses-update.php
@@ -9,7 +9,6 @@ $wa = $_POST['wa'];
 // memasukkan koneksi database
 include 'koneksi.php'
 
-
 if (isset($_POST['simpan'])) {
     $query = "UPDATE data_mhs SET nama='". $nama ."',jurusan='". $jurusan ."',email='". $email ."',wa='". $wa ."' WHERE id = " . $id;
 
